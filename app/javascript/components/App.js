1600aabbcc004594bb5e0dc42540a49d2ea24797
Samuellekpor <samuellekpor@gmail.com>
2023-04-04 20:24:03 +0000

    Create and render App component

---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const App = () => (
+    <BrowserRouter>
+      <Routes>
+        <h1>Hello World!</h1>
+      </Routes>
+    </BrowserRouter>
+);
+
+export default App;
