c89a4d845a10534117939a281ef4731de3e69207
Samuellekpor <samuellekpor@gmail.com>
2023-04-05 00:45:17 +0000

    Update index method

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,12 +1,5 @@
 class Api::V1::MessagesController < ApplicationController
   def index
-    rand_id = rand(1..Message.count)
-    @greeting = "Message.find(rand_id) if rand_id"
-
-    if @greeting
-      render json: @greeting, status: :ok
-    else
-      render json: { message: 'There is no message' }, status: :not_found
-    end
+    render json: Message.find(rand(1..5))
   end
 end
